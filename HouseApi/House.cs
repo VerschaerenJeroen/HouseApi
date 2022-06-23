cafae56bfac98e2348f90815d2e74a5895839d14
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2022-06-23 08:47:42 +0200

    Add project files.

---
+++ HouseApi/House.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseApi
+{
+    public class House
+    {
+        public int Id { get; set; }
+        public string Location { get; set; } = String.Empty;
+        public double Prize { get; set; }
+        public int Rooms { get; set; }
+    }
+}
