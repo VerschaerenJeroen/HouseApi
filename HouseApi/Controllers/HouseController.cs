c3fc915e0f85a2d2dc0815e4355168e228deacc4
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2022-06-23 08:57:57 +0200

    Added Controller for house

---
+++ HouseApi/Controllers/HouseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HouseController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var houses = new List<House>
+            {
+                new House {Id = 1, Location = "Mechelen", Prize = 1000.00, Rooms = 5}
+            };
+
+            return Ok(houses);
+        }
+    }
+}
